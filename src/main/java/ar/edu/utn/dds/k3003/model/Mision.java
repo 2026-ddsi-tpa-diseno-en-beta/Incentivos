22d09c863593509efb9a39fb498e2f5cea536f82
MelinaZingales <mzingales@frba.utn.edu.ar>
2026-08-14 19:36:14 -0300

    agrego columna de mision para borrar de la bdd

--- src/main/java/ar/edu/utn/dds/k3003/model/Mision.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Mision.java
@@ -32,6 +32,7 @@ public abstract class Mision {
 
     @Enumerated(EnumType.STRING)
     private CategoriaDonadorEnum categoriaFin;
+     private boolean completada= false;
 
     @Enumerated(EnumType.STRING)
     private TipoMisionEnum tipo;
